8862f7ab4290345b48f7d4a23f72f10e4c7635ff
SumitKumarSinghCode <inbox.sumit.ks@gmail.com>
2024-11-06 13:33:02 +0530

    update

---
+++ src/test/java/WindowActivities.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowActivities {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.co.in/"); // in this case selenium will wait for the page to get fully loaded.
+        driver.navigate().to("https://rahulshettyacademy.com/"); //in this case selenium will not wait for the page to get fully loaded.
+        Thread.sleep(2000);
+        driver.navigate().back();
+
+        driver.close();
+        System.out.println("All Test Case Passed");
+    }
+}
